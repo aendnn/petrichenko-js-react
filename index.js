6762af2c693160928c5637c679e7dc01a3a7bbd7
Anna <aendnn9@gmail.com>
2023-03-11 17:23:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/* eslint-disable prefer-template */
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < 3; i++) {
+  console.log('i ' + i);
+
+  for (let j = 0; j < 3; j++) {
+    console.log('j ' + j);
+  }
+}
